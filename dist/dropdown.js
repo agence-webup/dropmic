77158ff8f3c87ddd567e6386256fa1ef065d348b
Tixie <tixie@agence-webup.com>
2016-09-26 11:43:59 +0200

    Content generator is working

--- dist/dropdown.js
+++ dist/dropdown.js
@@ -1,4 +1,6 @@
-"use strict";
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -10,20 +12,169 @@ var Dropdown = function () {
 
         this.target = target;
         this.options = options;
+
+        this.init();
     }
 
     _createClass(Dropdown, [{
-        key: "setContent",
-        value: function setContent(content) {
-            console.log(content);
+        key: 'init',
+        value: function init() {
+            this._bindEvents();
         }
     }, {
-        key: "addLink",
-        value: function addLink(label, link) {}
+        key: '_bindEvents',
+        value: function _bindEvents() {
+            var that = this;
+            this.target.addEventListener("click", function () {
+                that._setContent(that.target);
+            });
+        }
     }, {
-        key: "addBtn",
-        value: function addBtn(label, callback) {}
+        key: '_getCustom',
+        value: function _getCustom(options) {
+            for (var key in options) {
+                if (options.hasOwnProperty(key) && key === '_addCustom') {
+                    var content = this._addCustom(this.options[key].content);
+                    return content;
+                }
+            }
+            return '';
+        }
+    }, {
+        key: '_getListItem',
+        value: function _getListItem(content) {
+            var listItem = document.createElement("li");
+            listItem.classList.add("dd-menu__listItem");
+            listItem.appendChild(content);
+            return listItem;
+        }
+    }, {
+        key: '_getList',
+        value: function _getList(options) {
+            var list = void 0;
+            if (_typeof(this.options) === 'object') {
+                list = document.createElement("ul");
+                list.classList.add("dd-menu__list");
+                for (var key in this.options) {
+                    if (this.options.hasOwnProperty(key)) {
+                        switch (key) {
+                            case '_addLink':
+                                list.appendChild(this._getListItem(this._addLink(this.options[key].label, this.options[key].link)));
+                                break;
+                            case '_addBtn':
+                                list.appendChild(this._getListItem(this._addBtn(this.options[key].label, this.options[key].callback)));
+                                break;
+                            case '_addLabel':
+                                list.appendChild(this._getListItem(this._addLabel(this.options[key].label)));
+                                break;
+                            default:
+                        }
+                    }
+                }
+            } else {
+                console.error('options type error');
+            }
+            return list;
+        }
+    }, {
+        key: '_addLink',
+        value: function _addLink(label, link) {
+            if (typeof label === 'string' && typeof link === 'string') {
+                var content = document.createElement("a");
+                content.classList.add("dd-menu__listContent");
+                content.setAttribute("href", link);
+                content.innerHTML = label;
+                return content;
+            } else {
+                console.error('_addLink type error');
+            }
+        }
+    }, {
+        key: '_addBtn',
+        value: function _addBtn(label, callback) {
+            if (typeof label === 'string' && typeof callback === 'function') {
+                var content = document.createElement("button");
+                content.classList.add("dd-menu__listContent");
+                content.innerHTML = label;
+                return content;
+            } else {
+                console.error('_addBtn type error');
+            }
+        }
+    }, {
+        key: '_addLabel',
+        value: function _addLabel(label) {
+            if (typeof label === 'string') {
+                var content = document.createElement("span");
+                content.classList.add("dd-menu__listContent");
+                content.innerHTML = label;
+                return content;
+            } else {
+                console.error('_addLabel type error');
+            }
+        }
+    }, {
+        key: '_addCustom',
+        value: function _addCustom(content) {
+            if (typeof content === 'string') {
+                var customContent = document.createElement("div");
+                customContent.classList.add("dd-menu__custom");
+                customContent.innerHTML = content;
+                return customContent;
+            } else {
+                console.error('_addCustom type error');
+            }
+        }
+    }, {
+        key: '_getContent',
+        value: function _getContent() {
+            var menu = document.createElement("div");
+            menu.classList.add("dd-menu");
+            var menuWrapper = document.createElement("div");
+            menuWrapper.classList.add("dd-menu__content");
+
+            menu.appendChild(menuWrapper);
+            if (_typeof(this._getCustom(this.options)) === "object") {
+                menuWrapper.appendChild(this._getCustom(this.options));
+            } else {}
+
+            if (_typeof(this._getList(this.options)) === "object") {
+                menuWrapper.appendChild(this._getList(this.options));
+            } else {}
+
+            return menu;
+        }
+    }, {
+        key: '_setContent',
+        value: function _setContent(item) {
+            item.appendChild(this._getContent());
+        }
+    }, {
+        key: '_removeContent',
+        value: function _removeContent(item) {
+            var lastItem = item.lastElementChild;
+            item.removeChild(lastItem);
+        }
     }]);
 
     return Dropdown;
 }();
+
+var plop = new Dropdown(document.querySelector('[data-dropdown]'), {
+    '_addLink': {
+        label: 'mon lien',
+        link: 'http://okokfsfd'
+    },
+    '_addBtn': {
+        label: 'mon bouton',
+        callback: function callback() {
+            console.log("fonction");
+        }
+    },
+    '_addLabel': {
+        label: 'mon label'
+    },
+    '_addCustom': {
+        content: 'super contenu custom'
+    }
+});
--- src/dropdown.js
+++ src/dropdown.js
@@ -3,17 +3,154 @@ class Dropdown {
     constructor(target, options) {
         this.target = target;
         this.options = options;
+
+        this.init();
     }
 
-    setContent(content) {
-        console.log(content);
+    init() {
+        this._bindEvents();
     }
 
-    addLink(label, link) {
-
+    _bindEvents() {
+        let that = this;
+        this.target.addEventListener("click", function() {
+            that._setContent(that.target);
+        });
     }
 
-    addBtn(label, callback) {
-
+    _getCustom(options) {
+        for (let key in options) {
+            if (options.hasOwnProperty(key) && key === '_addCustom') {
+                let content = this._addCustom(this.options[key].content);
+                return content;
+            }
+        }
+        return '';
     }
+
+    _getListItem(content) {
+        let listItem = document.createElement("li");
+        listItem.classList.add("dd-menu__listItem");
+        listItem.appendChild(content);
+        return listItem;
+    }
+
+    _getList(options) {
+        let list;
+        if (typeof this.options === 'object') {
+            list = document.createElement("ul");
+            list.classList.add("dd-menu__list");
+            for(let key in this.options) {
+                if (this.options.hasOwnProperty(key)) {
+                    switch (key) {
+                        case '_addLink':
+                            list.appendChild(this._getListItem(this._addLink(this.options[key].label, this.options[key].link)));
+                            break;
+                        case '_addBtn':
+                            list.appendChild(this._getListItem(this._addBtn(this.options[key].label, this.options[key].callback)));
+                            break;
+                        case '_addLabel':
+                            list.appendChild(this._getListItem(this._addLabel(this.options[key].label)));
+                            break;
+                        default:
+                    }
+                }
+            }
+        } else {
+            console.error('options type error');
+        }
+        return list;
+    }
+
+    _addLink(label, link) {
+        if (typeof label === 'string' && typeof link === 'string') {
+            let content = document.createElement("a");
+            content.classList.add("dd-menu__listContent");
+            content.setAttribute("href", link);
+            content.innerHTML = label;
+            return content;
+        } else {
+            console.error('_addLink type error');
+        }
+    }
+
+    _addBtn(label, callback) {
+        if (typeof label === 'string' && typeof callback === 'function') {
+            let content = document.createElement("button");
+            content.classList.add("dd-menu__listContent");
+            content.innerHTML = label;
+            return content;
+        } else {
+            console.error('_addBtn type error');
+        }
+    }
+
+    _addLabel(label) {
+        if (typeof label === 'string') {
+            let content = document.createElement("span");
+            content.classList.add("dd-menu__listContent");
+            content.innerHTML = label;
+            return content;
+        } else {
+            console.error('_addLabel type error');
+        }
+    }
+
+    _addCustom(content) {
+        if (typeof content === 'string') {
+            let customContent = document.createElement("div");
+            customContent.classList.add("dd-menu__custom");
+            customContent.innerHTML = content;
+            return customContent;
+        } else {
+            console.error('_addCustom type error');
+        }
+    }
+
+    _getContent() {
+        let menu = document.createElement("div");
+        menu.classList.add("dd-menu");
+        let menuWrapper = document.createElement("div");
+        menuWrapper.classList.add("dd-menu__content");
+
+        menu.appendChild(menuWrapper);
+        if (typeof this._getCustom(this.options) === "object") {
+            menuWrapper.appendChild(this._getCustom(this.options));
+        } else {}
+
+        if (typeof this._getList(this.options) === "object") {
+            menuWrapper.appendChild(this._getList(this.options));
+        } else {}
+
+        return menu;
+    }
+
+    _setContent(item) {
+        item.appendChild(this._getContent());
+    }
+
+    _removeContent(item) {
+        let lastItem = item.lastElementChild;
+        item.removeChild(lastItem);
+    }
+
 }
+
+let plop = new Dropdown(document.querySelector('[data-dropdown]'), {
+    '_addLink': {
+        label : 'mon lien',
+        link : 'http://okokfsfd'
+    },
+    '_addBtn': {
+        label : 'mon bouton',
+        callback : function() {
+            console.log("fonction");
+        }
+    },
+    '_addLabel': {
+        label : 'mon label'
+    },
+    '_addCustom': {
+        content : 'super contenu custom'
+    }
+});
